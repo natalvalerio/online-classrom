eccc845a7000ed419833f3ac24c91df1c3ee159d
natalvalerio <natalvalerio@gmail.com>
2022-03-14 20:54:43 -0300

    nova linha

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,9 @@ export default function Home(): JSX.Element {
   <div></div>
   <div className="py-20">
     <h1 className="text-5xl text-center text-accent-1">
-    Next.js + TailWind CSS Nova linha de divisão
+    Next.js + TailWind CSS 
+    <br/>Nova linha de divisão
+    <br/>Mais uma linha
     </h1>
   </div>
   {tail}
